d1f70f7e3e9c5d424c79a20a0ace6ece6435aead
YourtionGuo <yourtion@gmail.com>
2017-01-19 17:42:49 +0800

    todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,116 @@
+<template>
+  <el-row class="content">
+    <el-col :xs="{span: 20, offset: 2}" :sm="{span: 8, offset: 8}">
+      <span>欢迎：{{ name }} ！你的待办事项是：</span>
+      <el-input placeholder="请输入待办事项" v-model="todos" @keyup.enter.native="addTodos"></el-input>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="待办事项" name="first">
+          <el-col :xs="24">
+            <template v-if="!Done">
+              <template v-for="(item, index) in list">
+                <div class="todo-list" v-if="item.status === false">
+                  <span class="item">
+                    {{ index + 1 }}. {{ item.content }}
+                  </span>
+                  <span class="pull-right">
+                    <el-button size="small" type="primary" @click="finished(index)">完成</el-button>
+                    <el-button size="small" :plain="true" type="danger" @click="remove(index)">删除</el-button>
+                  </span>
+                </div>
+              </template>
+            </template>
+            <div v-else-if="Done">
+              暂无待办事项
+            </div>
+          </el-col>
+        </el-tab-pane>
+        <el-tab-pane label="已完成事项" name="second">
+          <template v-if="count > 0">
+            <template v-for="(item, index) in list">
+              <div class="todo-list" v-if="item.status == true">
+                <span class="item finished">
+                  {{ index + 1 }}. {{ item.content }}
+                </span>
+                <span class="pull-right">
+                  <el-button size="small" type="primary" @click="restore(index)">还原</el-button>
+                </span>
+              </div>
+            </template>
+          </template>
+          <div v-else>
+            暂无已完成事项
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Yourtion',
+      todos: '',
+      activeName: 'first',
+      list: [],
+      count: 0,
+    };
+  },
+  computed: {
+    Done() {
+      let count = 0;
+      const length = this.list.length;
+      for(const item of this.list) {
+        item.status === true ? count += 1 : '';
+      }
+      this.count = count;
+      if(count === length || length === 0) {
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {
+    addTodos() {
+      if(this.todos === '') return;
+      this.list.push({ status: false, content: this.todos });
+      this.todos = '';
+    },
+    finished(index) {
+      this.$set(this.list[index], 'status', true);
+      this.$message({ type: 'success', message: '任务完成' });
+    },
+    remove(index) {
+      this.list.splice(index, 1);
+      this.$message({ type: 'info', message: '任务删除' });
+    },
+    restore(index) {
+      this.$set(this.list[index], 'status', false);
+      this.$message({ type: 'info', message: '任务恢复' });
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .el-input
+    margin: 20px auto
+  .todo-list
+    width: 100%
+    margin-top: 8px
+    padding-bottom: 8px
+    border-bottom: 1px solid #eee
+    overflow: hidden
+    text-align: left
+    .item
+      font-size 20px
+      &.finished
+        text-decoration line-through
+        color #ddd
+  .pull-right
+    float: right
+</style>
+
+
+
